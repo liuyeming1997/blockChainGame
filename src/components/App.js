23522f45c86f5c25a0163511b1fc8c796a3dab00
Yeming Liu <yemingl@student.unimelb.edu.au>
2022-05-25 12:15:59 +0800

    update project all

--- src/components/App.js
+++ src/components/App.js
@@ -2,61 +2,7 @@ import React, { Component } from 'react';
 import Web3 from 'web3'
 import './App.css';
 import MemoryToken from '../abis/MemoryToken.json'
-import sha256 from 'crypto-js/sha256'
-import { v4 as uuidv4 } from "uuid"
-//import { message, Button, Space } from 'antd';
-import brain from '../brain.png'
-import { PanelGroup } from 'react-bootstrap';
-const CARD_ARRAY = [
-  {
-    name: 'fries',
-    img: '/images/fries.png'
-  },
-  {
-    name: 'cheeseburger',
-    img: '/images/cheeseburger.png'
-  },
-  {
-    name: 'ice-cream',
-    img: '/images/ice-cream.png'
-  },
-  {
-    name: 'pizza',
-    img: '/images/pizza.png'
-  },
-  {
-    name: 'milkshake',
-    img: '/images/milkshake.png'
-  },
-  {
-    name: 'hotdog',
-    img: '/images/hotdog.png'
-  },
-  {
-    name: 'fries',
-    img: '/images/fries.png'
-  },
-  {
-    name: 'cheeseburger',
-    img: '/images/cheeseburger.png'
-  },
-  {
-    name: 'ice-cream',
-    img: '/images/ice-cream.png'
-  },
-  {
-    name: 'pizza',
-    img: '/images/pizza.png'
-  },
-  {
-    name: 'milkshake',
-    img: '/images/milkshake.png'
-  },
-  {
-    name: 'hotdog',
-    img: '/images/hotdog.png'
-  }
-]
+
 class App extends Component {
   async componentWillMount() {
     await this.loadWeb3()
@@ -101,61 +47,6 @@ class App extends Component {
       alert('Smart contract not deployed to detected network.')
     }
   }
-  /*
-  chooseImage = (cardId) => {
-    cardId = cardId.toString()
-    if(this.state.cardsWon.includes(cardId)) {
-      return window.location.origin + '/images/white.png'
-    }
-    else if(this.state.cardsChosenId.includes(cardId)) {
-      return CARD_ARRAY[cardId].img
-    } else {
-      return window.location.origin + '/images/blank.png'
-    }
-  }
-
-  flipCard = async (cardId) => {
-    let alreadyChosen = this.state.cardsChosen.length
-
-    this.setState({
-      cardsChosen: [...this.state.cardsChosen, this.state.cardArray[cardId].name],
-      cardsChosenId: [...this.state.cardsChosenId, cardId]
-    })
-
-    if (alreadyChosen === 1) {
-      setTimeout(this.checkForMatch, 100)
-    }
-  }
-  checkForMatch = async () => {
-    const optionOneId = this.state.cardsChosenId[0]
-    const optionTwoId = this.state.cardsChosenId[1]
-
-    if(optionOneId == optionTwoId) {
-      alert('You have clicked the same image!')
-    } else if (this.state.cardsChosen[0] === this.state.cardsChosen[1]) {
-      alert('You found a match')
-      this.state.token.methods.mint(
-         this.state.account,
-         window.location.origin + CARD_ARRAY[optionOneId].img.toString()
-       )
-       .send({ from: this.state.account })
-       .on('transactionHash', (hash) => {
-         this.setState({
-           cardsWon: [...this.state.cardsWon, optionOneId, optionTwoId],
-           tokenURIs: [...this.state.tokenURIs, CARD_ARRAY[optionOneId].img]
-         })
-       })
-    } else {
-      alert('Sorry, try again')
-    }
-    this.setState({
-      cardsChosen: [],
-      cardsChosenId: []
-    })
-    if (this.state.cardsWon.length === CARD_ARRAY.length) {
-      alert('Congratulations! You found them all!')
-    }
-  }*/
 
   ///////////////////////////////////////////////////////////////////
   StartGame() {
@@ -262,26 +153,8 @@ class App extends Component {
     })
   }
   defineWinned = () => {
-    // const name = this.state.isClick == 1 ? '黑棋胜' : '白棋胜'
-    //  // this.state.token.methods.defineWinned(this.state.isClick - 1).
-    //  //  send({ from: this.state.account }).then(//console.log)
-    //  alert(name)
-     //console.log("winner1: ", this.state.isWinner)
      this.setState({isWinner: true})
-     //console.log("winner2: ", this.state.isWinner)
      return true;
-      /*this.state.token.methods.defineWinned(this.state.isClick - 1).send({value:2, from: this.state.account})
-      .on('transactionHash', function(hash){ 
-          this.setState({isStart: false})
-       })
-       .on('confirmation', function(confirmationNumber, receipt){  })
-       .on('receipt', function(receipt){
-       //console.log(receipt);
-   })
-   .on('error', function(error, receipt) { 
-   });*/
-  
-    
   }
 
   checkGameOver(index, indexs) {
@@ -650,136 +523,6 @@ class App extends Component {
       return;
   }
   }
-  /*
-  handleClick(index, indexs) {
-    let state = this.state.isClickArr.findIndex((n) => n.idx == index && n.idxs == indexs)
-    if (state != -1) {
-        return
-    }
-    this.setState({
-        data: this.state.data + 1,
-        idx: index,
-        idxs: indexs,
-        isClick: this.state.data % 2 === 0 ? 1 : 2,
-        isClickArr: this.state.isClickArr.concat([{
-            data: this.state.data + 1,
-            idx: index,
-            idxs: indexs,
-            isClick: this.state.data % 2 === 0 ? 1 : 2
-        }])
-    }, () => {
-        let letArr = this.state.twoArray.map((ele, index1) => {
-            let arr = Array(20).fill([])
-            let arrr=arr.map((item,row) => {
-                arr[this.state.idx] = this.state.isClickArr.filter((eles, index2) => {
-                    return index1 == eles.idxs && eles.idx == row
-                })
-                let arrData = arr[this.state.idx].length > 0 ? arr[this.state.idx][0] : ''
-                return arrData
-            })
-            let arrs = Array(20).fill([])
-            arrs[indexs][index] = arrr
-            return arrs[indexs][index]
-        })
-        // (纵坐标，横坐标)[indexs][index]确定一个点的位置
-        //console.log(letArr) // 按第一行、第二行...第二十行 纵坐标
-        //console.log(index, 'index') // 横坐标
-        //console.log(indexs, 'indexs') // 纵坐标
-        //列计数
-        let columnCount = 0;
-        // 向上下棋
-        for (let i = indexs + 1; i < 20; i++) {
-            if (letArr[i][index].isClick == this.state.isClick) {
-                columnCount++;
-            } else {
-                break;
-            }
-        }
-        // 向下下棋
-        for (let i = indexs - 1; i >= 0; i--) {
-            if (letArr[i][index].isClick == this.state.isClick) {
-                columnCount++;
-            } else {
-                break;
-            }
-        }
-        if (columnCount >= 4) {
-            this.defineWinned()
-            columnCount = 0
-            return;
-        }
-        //行计数
-        let lineCount = 0;
-        // 向左下棋
-        for (let i = index + 1; i < 20; i++) {
-            if (letArr[indexs][i].isClick == this.state.isClick) {
-                lineCount++;
-            } else {
-                break;
-            }
-        }
-        // 向右下棋
-        for (let i = index - 1; i >= 0; i--) {
-            if (letArr[indexs][i].isClick == this.state.isClick) {
-                lineCount++;
-            } else {
-                break;
-            }
-        }
-        if (lineCount >= 4) {
-            this.defineWinned()
-            lineCount = 0
-            return;
-        }
-        //斜行计数-左斜 \
-        let obliqueLeftCount = 0;
-        // 向左上下棋↖
-        for (let i = index + 1, j = indexs + 1; i < 20 && j < 20; i++,j++) {
-            if (letArr[i][j].isClick == this.state.isClick) {
-                obliqueLeftCount++;
-            } else {
-                break;
-            }
-        }
-        // 向左下下棋↘
-        for (let i = index - 1, j = indexs - 1; i >= 0 && j >= 0; i--,j--) {
-            if (letArr[j][i].isClick == this.state.isClick) {
-                obliqueLeftCount++;
-            } else {
-                break;
-            }
-        }
-        if (obliqueLeftCount >= 4) {
-            this.defineWinned()
-            obliqueLeftCount = 0
-            return;
-        }
-        //斜行计数-右斜 /
-        let obliqueRightCount = 0;
-        // 向右上下棋↗
-        for (let i = indexs + 1, j = index - 1; i < 20 && j >= 0; i++,j--) {
-            if (letArr[i][j].isClick == this.state.isClick) {
-                obliqueRightCount++;
-            } else {
-                break;
-            }
-        }
-        // 向左右下下棋↙
-        for (let i = indexs- 1, j = index + 1; i >= 0 && j < 20; i--,j++) {
-            if (letArr[i][j].isClick == this.state.isClick) {
-                obliqueRightCount++;
-            } else {
-                break;
-            }
-        }
-        //console.log(obliqueRightCount,'obliqueRightCount')
-        if (obliqueRightCount >= 4) {
-            this.defineWinned()
-            obliqueRightCount = 0
-            return;
-        }
-    })
-} */
 
   constructor(props) {
     super(props)
@@ -797,14 +540,14 @@ class App extends Component {
       ///////////////////////
       arr: Array(20).fill(null),
       isClick: null,
-      idx: 0, // 点击点的的横坐标
-      idxs: 0, // 点击点的纵坐标
+      idx: 0, 
+      idxs: 0, 
       isClickArr: [{
                 idx: null,
                 idxs: null,
                 isClick: null
-      }], // 存放点击过的点的数组
-      twoArray: Array(20).fill([]), // 存放点击过的点的数组
+      }], 
+      twoArray: Array(20).fill([]), 
       arrs: [],
       isMyTurn: true,
       isStart: false,
@@ -821,14 +564,12 @@ class App extends Component {
 
   render() {
     var self = this;
-    //this.updateClickArr();
     return (
       
       <div className='main'>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-             //href="http://www.dappuniversity.com/bootcamp"
             target="_blank"
             rel="noopener noreferrer"
           >
